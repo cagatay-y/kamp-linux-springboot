57e3041770d1385b423cb4342673403250b9a4f4
Çağatay Yiğit Şahin <cyigitsahin@outlook.com>
2018-07-07 20:12:03 +0300

    Initialize from Spring Initializr

---
+++ src/main/java/com/github/cagatayy/kamplinux/KampLinuxApplication.java
@@ -0,0 +1,12 @@
+package com.github.cagatayy.kamplinux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KampLinuxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KampLinuxApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/cagatayy/kamplinux/KampLinuxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.cagatayy.kamplinux;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KampLinuxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
